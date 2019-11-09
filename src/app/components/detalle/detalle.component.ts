2c6b9d78337858d5b64c8a0c547ac4fc5a625811
04 <KMMX_miniMac05@DMN-wifi.local>
2019-11-08 18:08:14 -0600

    Cambios

--- src/app/components/detalle/detalle.component.ts
+++ src/app/components/detalle/detalle.component.ts
@@ -1,7 +1,9 @@
 import { Component, OnInit } from '@angular/core';
-import { Router, ActivatedRoute} from '@angular/router'
+import { Router, ActivatedRoute } from '@angular/router'
 import { Personaje } from "./../../interfaces/personaje";
 import { PersonajeService } from './../../services/personaje.service';
+
+
 @Component({
   selector: 'app-detalle',
   templateUrl: './detalle.component.html',
@@ -10,24 +12,20 @@ import { PersonajeService } from './../../services/personaje.service';
 export class DetalleComponent implements OnInit {
 
   private personajes: Personaje
-  constructor(private route: ActivatedRoute, private personajeServices: PersonajeService) { 
-    
-  }
+  constructor(private route: ActivatedRoute, private personajeServices: PersonajeService) {
 
+  }
   ngOnInit() {
     console.log(this.route.snapshot.paramMap.get('id'));
-    try{
-        this.personajeServices.getPersonaje(this.route.snapshot.paramMap.get('id'))
-                .subscribe((personaje:any)=>{
-                  console.log(personaje.data)
-                  if(personaje && personaje.data) this.personajes = personaje.data
-              })
-    }catch(e){
+    try {
+      this.personajeServices.getPersonaje(this.route.snapshot.paramMap.get('id'))
+        .subscribe((personaje: any) => {
+          
+          if (personaje && personaje.data) this.personajes = personaje.data
+        })
+    } catch (e) {
       console.log(e)
     }
-    
-    //this.personaje = this.personajeServices.getPersonaje(this.route.snapshot.paramMap.get('id'))
-    
   }
 
 }
